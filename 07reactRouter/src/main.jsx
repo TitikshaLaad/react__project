f405f0bb46402c13084b4c53a2049847b9a1d628
Titiksha Laad <titiksha.laad@gmail.com>
2026-02-03 19:35:38 +0530

    comitted Successfully

---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element:<Layout/>,
+    childern : [{
+      path:"",
+      element:<Home/>
+    }]
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router ={router}/>
+
+  </StrictMode>,
+)
